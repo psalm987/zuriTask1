7e43670a6fa4f672cc5857b0551b43e98cb6b551
samrythmn@gmail.com <samrythmn@gmail.com>
2022-11-01 01:36:21 +0100

    Rephrase reference

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Footer from "sections/footer.js";
-import LinkSection from "sections/linksection.js";
-import ProfileSection from "sections/profilesection.js";
+import Footer from "./sections/footer.js";
+import LinkSection from "./sections/linksection.js";
+import ProfileSection from "./sections/profilesection.js";
 import "./styles.css";
 
 export default function App() {
