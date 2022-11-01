42c60879ff5bb000eee6d81fd12f570792c354a1
samrythmn@gmail.com <samrythmn@gmail.com>
2022-11-01 01:33:54 +0100

    change reference

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Footer from "./sections/footer";
-import LinkSection from "./sections/linksection";
-import ProfileSection from "./sections/profilesection";
+import Footer from "sections/footer.js";
+import LinkSection from "sections/linksection.js";
+import ProfileSection from "sections/profilesection.js";
 import "./styles.css";
 
 export default function App() {
